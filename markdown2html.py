30621b44ac4c3bf1a04bbd0b4b56e049734807d9
arturadamian <arturadamian@gmail.com>
2020-02-16 22:17:56 -0800

    if name==main

--- markdown2html.py
+++ markdown2html.py
@@ -1,13 +1,13 @@
 #!/usr/local/bin/python3
 import sys
-import os.path
 from os import path
 
-if len(sys.argv) < 3:
-    sys.stdout.write("Usage: ./markdown2html.py README.md README.html\n")
-    exit (1)
-elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
-    sys.stdout.write("Missing " + sys.argv[1] + "\n")
-    exit (1)
-else:
-    exit(0)
+if __name__=='__main__':
+    if len(sys.argv) < 3:
+        sys.stdout.write("Usage: ./markdown2html.py README.md README.html\n")
+        exit (1)
+    elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
+        sys.stdout.write("Missing " + sys.argv[1] + "\n")
+        exit (1)
+    else:
+        exit(0)
