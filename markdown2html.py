6bafde9be945f2d1284047afba0bc437f52630d2
arturadamian <arturadamian@gmail.com>
2020-02-16 22:01:18 -0800

    exit 0

--- markdown2html.py
+++ markdown2html.py
@@ -6,6 +6,8 @@ from os import path
 if len(sys.argv) < 2:
     sys.stdout.write("Usage: ./markdown2html.py README.md README.html\n")
     exit (1)
-if not path.exists(sys.argv[1]):
+elif not path.exists(sys.argv[1]):
     print("Missing ", sys.argv[1], file=sys.stderr)
     exit (1)
+else:
+    exit(0)
