f56dc2b166dc39a21ea1dd0fa6c2cfe9b2078038
arturadamian <arturadamian@gmail.com>
2020-02-16 22:33:04 -0800

    else if statement change

--- markdown2html.py
+++ markdown2html.py
@@ -13,8 +13,7 @@ if __name__=='__main__':
     if len(sys.argv) < 3:
         sys.stdout.write("Usage: ./markdown2html.py README.md README.html\n")
         exit (1)
-    elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
+    if not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
         sys.stdout.write("Missing " + sys.argv[1] + "\n")
         exit (1)
-    else:
-        exit(0)
+    exit(0)
