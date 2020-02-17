cc22265fab900fa49aa7c2713dc74549d88d9dec
arturadamian <arturadamian@gmail.com>
2020-02-16 22:15:50 -0800

    check the extension of the file

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ from os import path
 if len(sys.argv) < 3:
     sys.stdout.write("Usage: ./markdown2html.py README.md README.html\n")
     exit (1)
-elif not path.exists(sys.argv[1]):
+elif not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
     sys.stdout.write("Missing " + sys.argv[1] + "\n")
     exit (1)
 else:
