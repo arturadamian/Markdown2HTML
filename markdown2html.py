c18c9d435e39303d22f809675dddf0045d49b766
arturadamian <arturadamian@gmail.com>
2020-02-23 23:59:51 -0800

    i

--- markdown2html.py
+++ markdown2html.py
@@ -9,12 +9,21 @@
 if __name__=='__main__':
     import sys
     from os import path
-
+    
+    mkd = {"#": "<h1>", "##": "<h2>", "###": "<h3>", "####": "<h4>", "#####": "<h5>", "######": "<h6>",}
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit (1)
     if not path.exists(sys.argv[1]) or not sys.argv[1].endswith('.md'):
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit (1)
-    with open(sys.argv[1], mode="r") as f:
+    with open(sys.argv[1], mode="r") as x, open(sys.argv[2], mode="w") as y:
+        for line in x:
+            words = line.split(" ")
+            if words[0] in mkd:
+                htmlTag = mkd[words[0]]
+            htmlFile = line.replace(words[0], "{}".format(htrmlTag))
+            htmlTag = htmlTag[:-1] + (" {}\n".format(htmlTag))
+            y.write(htmlTag)
+
         exit(0)
